018db0ecc2666bd3019f8b18ada5ac3fcef1dd51
Rishika Verma <rv5557@srmist.edu.in>
2025-08-12 17:14:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import folium
 from streamlit_folium import st_folium
-import pandas as pd
+import pandas as pd  
 import numpy as np  
 from datetime import datetime 
 import time 
